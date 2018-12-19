93814b19173cc4a97f61c9b0040d602f1f91053e
Jordan Larios <jolarios96@gmail.com>
2018-12-18 22:36:28 -0800

    some initial lines

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+// ROUTES-----------------------------------------------
+
+app.get("/public/home", function (req, res) {
+});
+
+app.get("/public/survey", function (req, res) {
+});
